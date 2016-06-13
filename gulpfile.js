7e86830841e70b2d35c28ad86f669b0c876c2bbc
Max Mumford <maxmumford@gmail.com>
2016-06-13 17:19:12 +0100

    Add browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var server = require('gulp-express');
-var shell = require('gulp-shell');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var exec = require('child_process').exec;
 
 // launch server
 gulp.task('server', function () {
@@ -11,13 +13,34 @@ gulp.task('server', function () {
     gulp.watch(['app.js'], server.notify);
 });
 
-// compile typscripts
-gulp.task('compile', shell.task([
-  'npm run tsc'
-]));
+// compile typescripts 
+gulp.task('compile', function (cb) {
+  exec('npm run tsc', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+    gulp.start('reload');
+  });
+})
 
+// reload browsersync
+gulp.task('reload', function(){
+  reload();
+});
+
+// start browser sync
+gulp.task('browser-sync', function() {
+  browserSync.init(null, {
+    proxy: "http://localhost:3000",
+        browser: "google chrome",
+        port: 8000,
+  });
+});
+
+// watch for file changes
 gulp.task('watch', ['compile'], function() {
   gulp.watch('app/**/*.ts', ['compile']);
 });
 
-gulp.task('default', ['server', 'watch']);
+// run everything
+gulp.task('default', ['server', 'watch', 'browser-sync']);
